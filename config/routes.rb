1a0ca52c9e03893361c670c3b8ad93d7197bbd3b
izszzz <izszzz@yahoo.co.jp>
2022-04-15 21:39:48 +0900

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages/home'
+  devise_for :users
+  resources :users
+  root 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
 end

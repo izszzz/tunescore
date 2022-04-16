27f652626465c3b23be9df5d7d3e87f8854ed963
izszzz <izszzz@yahoo.co.jp>
2022-04-17 04:21:01 +0900

    update sign_form & udpate user migration

--- db/migrate/20220415122505_devise_create_users.rb
+++ db/migrate/20220415122505_devise_create_users.rb
@@ -32,12 +32,26 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## User Info
+      # t.string :name
+      t.string :nickname, default: '', null: false
+      t.string :image
+
+      t.string :familyname, default: '', null: false
+      t.string :givenname, default: '', null: false
+      t.string :introduction, default: '', null: false
+      t.date :birthday
+      t.integer :gender
+      t.integer :following_count, default: 0, null: false
+      t.integer :followers_count, default: 0, null: false
+
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :nickname, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_122505) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "nickname", default: "", null: false
+    t.string "image"
+    t.string "familyname", default: "", null: false
+    t.string "givenname", default: "", null: false
+    t.string "introduction", default: "", null: false
+    t.date "birthday"
+    t.integer "gender"
+    t.integer "following_count", default: 0, null: false
+    t.integer "followers_count", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 

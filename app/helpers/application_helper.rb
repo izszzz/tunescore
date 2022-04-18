4026939ef816cea331bca09b080ba85a54358c5c
izszzz <izszzz@yahoo.co.jp>
2022-04-18 11:35:15 +0900

    add icon & add svg_icon_tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def svg_use_tag(name, **attributes)
+		path = asset_path("svg/#{name}.svg")
+		tag.svg **attributes do
+			tag.use "xlink:href": "#{path}##{name}"
+		end
+	end
 end

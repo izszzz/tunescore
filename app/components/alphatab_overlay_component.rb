f4db7c94bc966d3f79dc853a042cd39a463bf4ed
izszzz <izszzz@yahoo.co.jp>
2022-06-12 10:33:46 +0900

    add style

---
+++ app/components/alphatab_overlay_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class AlphatabOverlayComponent < ViewComponent::Base
+
+end
---
+++ app/components/alphatab_tracks_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class AlphatabTracksComponent < ViewComponent::Base
+
+end

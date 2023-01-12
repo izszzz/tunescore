449682f22d3917782608e563d0cb43d3b57076e9
k.yasoda <k.yasoda@pid-corp.jp>
2023-01-12 16:53:21 +0000

    chore: fix pagination

--- src/server/routers/pagination.ts
+++ src/server/routers/pagination.ts
@@ -9,169 +9,179 @@ import { UserFindManySchema } from "../../../prisma/generated/schemas/findManyUs
 import { AlbumFindManySchema } from "../../../prisma/generated/schemas/findManyAlbum.schema";
 import { FollowFindManySchema } from "../../../prisma/generated/schemas/findManyFollow.schema";
 import { BookmarkFindManySchema } from "../../../prisma/generated/schemas/findManyBookmark.schema";
-import { createRouter } from "../context";
+import { router, publicProcedure } from "../trpc";
 import type { MusicListQueryType } from "../../helpers/music";
 import type { BandListQueryType } from "../../helpers/band";
 import type { ArtistListQueryType } from "../../helpers/artist";
 import type { AlbumListQueryType } from "../../helpers/album";
 import type { Prisma } from "@prisma/client";
 
-// export const paginationRouter = createRouter()
-//   .query("music", {
-//     input: z.object({
-//       options: PaginateOptionsSchema,
-//       args: MusicFindManySchema,
-//     }),
-//     async resolve({ ctx, input }) {
-//       const { args, options } = input;
-//       const paginate = createPaginator(options);
-//       const data = await paginate<
-//         Prisma.MusicGetPayload<MusicListQueryType>,
-//         Prisma.MusicFindManyArgs
-//       >(ctx.prisma.music, args);
-//       return { type: "music" as const, ...data };
-//     },
-//   })
-//   .query("artist", {
-//     input: z.object({
-//       options: PaginateOptionsSchema,
-//       args: ArtistFindManySchema,
-//     }),
-//     async resolve({ ctx, input }) {
-//       const { args, options } = input;
-//       const paginate = createPaginator(options);
-//       const data = await paginate<
-//         Prisma.ArtistGetPayload<ArtistListQueryType>,
-//         Prisma.ArtistFindManyArgs
-//       >(ctx.prisma.artist, args);
-//       return { type: "artist" as const, ...data };
-//     },
-//   })
-//   .query("band", {
-//     input: z.object({
-//       options: PaginateOptionsSchema,
-//       args: BandFindManySchema,
-//     }),
-//     async resolve({ ctx, input }) {
-//       const { args, options } = input;
-//       const paginate = createPaginator(options);
-//       const data = await paginate<
-//         Prisma.BandGetPayload<BandListQueryType>,
-//         Prisma.BandFindManyArgs
-//       >(ctx.prisma.band, args);
-//       return { type: "band" as const, ...data };
-//     },
-//   })
-//   .query("album", {
-//     input: z.object({
-//       options: PaginateOptionsSchema,
-//       args: AlbumFindManySchema,
-//     }),
-//     async resolve({ ctx, input }) {
-//       const { args, options } = input;
-//       const paginate = createPaginator(options);
-//       const data = await paginate<
-//         Prisma.AlbumGetPayload<AlbumListQueryType>,
-//         Prisma.AlbumFindManyArgs
-//       >(ctx.prisma.album, args);
-//       return { type: "album" as const, ...data };
-//     },
-//   })
-//   .query("user", {
-//     input: z.object({
-//       options: PaginateOptionsSchema,
-//       args: UserFindManySchema,
-//     }),
-//     async resolve({ ctx, input }) {
-//       const { args, options } = input;
-//       const paginate = createPaginator(options);
-//       return await paginate<
-//         Prisma.UserGetPayload<{
-//           include: {
-//             _count: { select: { following: true; followers: true } };
-//           };
-//         }>,
-//         Prisma.UserFindManyArgs
-//       >(ctx.prisma.user, args);
-//     },
-//   })
-//   .query("issue", {
-//     input: z.object({
-//       options: PaginateOptionsSchema,
-//       args: PullFindManySchema,
-//     }),
-//     async resolve({ ctx, input }) {
-//       const { args, options } = input;
-//       const paginate = createPaginator(options);
-//       return await paginate<
-//         Prisma.IssueGetPayload<{
-//           include: { user: true };
-//         }>,
-//         Prisma.IssueFindManyArgs
-//       >(ctx.prisma.issue, args);
-//     },
-//   })
-//   .query("pull", {
-//     input: z.object({
-//       options: PaginateOptionsSchema,
-//       args: PullFindManySchema,
-//     }),
-//     async resolve({ ctx, input }) {
-//       const { args, options } = input;
-//       const paginate = createPaginator(options);
-//       return await paginate<
-//         Prisma.PullGetPayload<{
-//           include: { user: true };
-//         }>,
-//         Prisma.PullFindManyArgs
-//       >(ctx.prisma.pull, args);
-//     },
-//   })
-//   .query("follow", {
-//     input: z.object({
-//       options: PaginateOptionsSchema,
-//       args: FollowFindManySchema,
-//     }),
-//     async resolve({ ctx, input }) {
-//       const { args, options } = input;
-//       const paginate = createPaginator(options);
-//       return await paginate<
-//         Prisma.FollowGetPayload<{
-//           include: {
-//             follower: {
-//               include: {
-//                 _count: { select: { following: true; followers: true } };
-//               };
-//             };
-//             following: {
-//               include: {
-//                 _count: { select: { following: true; followers: true } };
-//               };
-//             };
-//           };
-//         }>,
-//         Prisma.FollowFindManyArgs
-//       >(ctx.prisma.follow, args);
-//     },
-//   })
-//   .query("bookmark", {
-//     input: z.object({
-//       options: PaginateOptionsSchema,
-//       args: BookmarkFindManySchema,
-//     }),
-//     async resolve({ ctx, input }) {
-//       const { args, options } = input;
-//       const paginate = createPaginator(options);
-//       return await paginate<
-//         Prisma.BookmarkGetPayload<{
-//           include: {
-//             music: MusicListQueryType;
-//             band: BandListQueryType;
-//             artist: ArtistListQueryType;
-//             album: AlbumListQueryType;
-//           };
-//         }>,
-//         Prisma.BookmarkFindManyArgs
-//       >(ctx.prisma.bookmark, args);
-//     },
-//   });
+export const paginationRouter = router({
+  music: publicProcedure
+    .input(
+      z.object({
+        options: PaginateOptionsSchema,
+        args: MusicFindManySchema,
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { args, options } = input;
+      const paginate = createPaginator(options);
+      const data = await paginate<
+        Prisma.MusicGetPayload<MusicListQueryType>,
+        Prisma.MusicFindManyArgs
+      >(ctx.prisma.music, args);
+      return { type: "music" as const, ...data };
+    }),
+  artist: publicProcedure
+    .input(
+      z.object({
+        options: PaginateOptionsSchema,
+        args: ArtistFindManySchema,
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { args, options } = input;
+      const paginate = createPaginator(options);
+      const data = await paginate<
+        Prisma.ArtistGetPayload<ArtistListQueryType>,
+        Prisma.ArtistFindManyArgs
+      >(ctx.prisma.artist, args);
+      return { type: "artist" as const, ...data };
+    }),
+  band: publicProcedure
+    .input(
+      z.object({
+        options: PaginateOptionsSchema,
+        args: BandFindManySchema,
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { args, options } = input;
+      const paginate = createPaginator(options);
+      const data = await paginate<
+        Prisma.BandGetPayload<BandListQueryType>,
+        Prisma.BandFindManyArgs
+      >(ctx.prisma.band, args);
+      return { type: "band" as const, ...data };
+    }),
+  album: publicProcedure
+    .input(
+      z.object({
+        options: PaginateOptionsSchema,
+        args: AlbumFindManySchema,
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { args, options } = input;
+      const paginate = createPaginator(options);
+      const data = await paginate<
+        Prisma.AlbumGetPayload<AlbumListQueryType>,
+        Prisma.AlbumFindManyArgs
+      >(ctx.prisma.album, args);
+      return { type: "album" as const, ...data };
+    }),
+  user: publicProcedure
+    .input(
+      z.object({
+        options: PaginateOptionsSchema,
+        args: UserFindManySchema,
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { args, options } = input;
+      const paginate = createPaginator(options);
+      return await paginate<
+        Prisma.UserGetPayload<{
+          include: {
+            _count: { select: { following: true; followers: true } };
+          };
+        }>,
+        Prisma.UserFindManyArgs
+      >(ctx.prisma.user, args);
+    }),
+  issue: publicProcedure
+    .input(
+      z.object({
+        options: PaginateOptionsSchema,
+        args: PullFindManySchema,
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { args, options } = input;
+      const paginate = createPaginator(options);
+      return await paginate<
+        Prisma.IssueGetPayload<{
+          include: { user: true };
+        }>,
+        Prisma.IssueFindManyArgs
+      >(ctx.prisma.issue, args);
+    }),
+  pull: publicProcedure
+    .input(
+      z.object({
+        options: PaginateOptionsSchema,
+        args: PullFindManySchema,
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { args, options } = input;
+      const paginate = createPaginator(options);
+      return await paginate<
+        Prisma.PullGetPayload<{
+          include: { user: true };
+        }>,
+        Prisma.PullFindManyArgs
+      >(ctx.prisma.pull, args);
+    }),
+  follow: publicProcedure
+    .input(
+      z.object({
+        options: PaginateOptionsSchema,
+        args: FollowFindManySchema,
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { args, options } = input;
+      const paginate = createPaginator(options);
+      return await paginate<
+        Prisma.FollowGetPayload<{
+          include: {
+            follower: {
+              include: {
+                _count: { select: { following: true; followers: true } };
+              };
+            };
+            following: {
+              include: {
+                _count: { select: { following: true; followers: true } };
+              };
+            };
+          };
+        }>,
+        Prisma.FollowFindManyArgs
+      >(ctx.prisma.follow, args);
+    }),
+  bookmark: publicProcedure
+    .input(
+      z.object({
+        options: PaginateOptionsSchema,
+        args: BookmarkFindManySchema,
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { args, options } = input;
+      const paginate = createPaginator(options);
+      return await paginate<
+        Prisma.BookmarkGetPayload<{
+          include: {
+            music: MusicListQueryType;
+            band: BandListQueryType;
+            artist: ArtistListQueryType;
+            album: AlbumListQueryType;
+          };
+        }>,
+        Prisma.BookmarkFindManyArgs
+      >(ctx.prisma.bookmark, args);
+    }),
+});

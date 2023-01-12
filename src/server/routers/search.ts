e0c8ad01eb3a0dd5572238155244a25e67fd06b1
k.yasoda <k.yasoda@pid-corp.jp>
2023-01-12 17:00:28 +0000

    chore: fix search api

--- src/server/routers/search.ts
+++ src/server/routers/search.ts
@@ -9,125 +9,112 @@ import { TagFindManySchema } from "../../../prisma/generated/schemas/findManyTag
 import { BookmarkFindManySchema } from "../../../prisma/generated/schemas/findManyBookmark.schema";
 import { FollowFindManySchema } from "../../../prisma/generated/schemas/findManyFollow.schema";
 import { RoleFindManySchema } from "../../../prisma/generated/schemas/findManyRole.schema";
-import { createRouter } from "../context";
+import { publicProcedure, router } from "../trpc";
 
-// export const searchRouter = createRouter()
-//   .mutation("music", {
-//     input: MusicFindManySchema,
-//     async resolve({ ctx, input }) {
-//       return ctx.prisma.music.findMany(input);
-//     },
-//   })
-//   .mutation("artist", {
-//     input: ArtistFindManySchema,
-//     async resolve({ ctx, input }) {
-//       return ctx.prisma.artist.findMany(input);
-//     },
-//   })
-//   .mutation("band", {
-//     input: BandFindManySchema,
-//     async resolve({ ctx, input }) {
-//       return ctx.prisma.band.findMany(input);
-//     },
-//   })
-//   .mutation("album", {
-//     input: AlbumFindManySchema,
-//     async resolve({ ctx, input }) {
-//       return ctx.prisma.album.findMany(input);
-//     },
-//   })
-//   .mutation("user", {
-//     input: UserFindManySchema,
-//     async resolve({ ctx, input }) {
-//       return ctx.prisma.user.findMany(input);
-//     },
-//   })
-//   .mutation("issue", {
-//     input: IssueFindManySchema,
-//     async resolve({ ctx, input }) {
-//       return ctx.prisma.issue.findMany(input);
-//     },
-//   })
-//   .mutation("pull", {
-//     input: PullFindManySchema,
-//     async resolve({ ctx, input }) {
-//       return ctx.prisma.pull.findMany(input);
-//     },
-//   })
-//   .mutation("tag", {
-//     input: TagFindManySchema,
-//     async resolve({ ctx, input }) {
-//       return ctx.prisma.tag.findMany(input);
-//     },
-//   })
-//   .mutation("role", {
-//     input: RoleFindManySchema,
-//     async resolve({ ctx, input }) {
-//       return ctx.prisma.role.findMany(input);
-//     },
-//   })
-//   .mutation("bookmark", {
-//     input: BookmarkFindManySchema,
-//     async resolve({ ctx, input }) {
-//       return ctx.prisma.bookmark.findMany({
-//         ...input,
-//         include: {
-//           music: {
-//             include: {
-//               user: true,
-//               band: true,
-//               bookmarks: true,
-//               _count: {
-//                 select: {
-//                   bookmarks: true,
-//                 },
-//               },
-//             },
-//           },
-//           album: {
-//             include: {
-//               band: true,
-//               _count: {
-//                 select: {
-//                   musics: true,
-//                   bookmarks: true,
-//                   artists: true,
-//                 },
-//               },
-//             },
-//           },
-//           band: {
-//             include: {
-//               _count: {
-//                 select: {
-//                   bookmarks: true,
-//                   artists: true,
-//                   musics: true,
-//                   albums: true,
-//                 },
-//               },
-//             },
-//           },
-//           artist: {
-//             include: {
-//               bands: true,
-//               _count: {
-//                 select: {
-//                   bookmarks: true,
-//                 },
-//               },
-//             },
-//           },
-//         },
-//       });
-//     },
-//   })
-//   .mutation("follow", {
-//     input: FollowFindManySchema,
-//     async resolve({ ctx, input }) {
-//       return ctx.prisma.follow.findMany({
-//         ...input,
-//         include: { follower: true, following: true },
-//       });
-//     },
-//   });
+export const searchRouter = router({
+  music: publicProcedure
+    .input(MusicFindManySchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.music.findMany(input);
+    }),
+  artist: publicProcedure
+    .input(ArtistFindManySchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.artist.findMany(input);
+    }),
+  band: publicProcedure
+    .input(BandFindManySchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.band.findMany(input);
+    }),
+  album: publicProcedure
+    .input(AlbumFindManySchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.album.findMany(input);
+    }),
+  user: publicProcedure
+    .input(UserFindManySchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.user.findMany(input);
+    }),
+  issue: publicProcedure
+    .input(IssueFindManySchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.issue.findMany(input);
+    }),
+  pull: publicProcedure
+    .input(PullFindManySchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.pull.findMany(input);
+    }),
+  tag: publicProcedure
+    .input(TagFindManySchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.tag.findMany(input);
+    }),
+  role: publicProcedure
+    .input(RoleFindManySchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.role.findMany(input);
+    }),
+  follow: publicProcedure
+    .input(FollowFindManySchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.follow.findMany(input);
+    }),
+  bookmark: publicProcedure
+    .input(BookmarkFindManySchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.bookmark.findMany({
+        ...input,
+        include: {
+          music: {
+            include: {
+              user: true,
+              band: true,
+              bookmarks: true,
+              _count: {
+                select: {
+                  bookmarks: true,
+                },
+              },
+            },
+          },
+          album: {
+            include: {
+              band: true,
+              _count: {
+                select: {
+                  musics: true,
+                  bookmarks: true,
+                  artists: true,
+                },
+              },
+            },
+          },
+          band: {
+            include: {
+              _count: {
+                select: {
+                  bookmarks: true,
+                  artists: true,
+                  musics: true,
+                  albums: true,
+                },
+              },
+            },
+          },
+          artist: {
+            include: {
+              bands: true,
+              _count: {
+                select: {
+                  bookmarks: true,
+                },
+              },
+            },
+          },
+        },
+      });
+    }),
+});

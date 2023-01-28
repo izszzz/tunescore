33f856301b9a17b9b133307289e6050701e9487a
k.yasoda <k.yasoda@pid-corp.jp>
2023-01-28 19:23:48 +0000

    Merge branch 'main' into develop

--- next.config.mjs
+++ next.config.mjs
@@ -32,13 +32,5 @@ export default defineNextConfig({
   },
   eslint: {
     dirs: ["src/"],
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
   },
 });

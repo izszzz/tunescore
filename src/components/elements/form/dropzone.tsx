8efc60d8e8d717c549a32599dc62c13111ca1e19
k.yasoda <k.yasoda@pid-corp.jp>
2023-02-13 07:16:43 +0000

    feat: add dropzone

---
+++ src/components/elements/form/dropzone.tsx
@@ -0,0 +1,37 @@
+import type { DropzoneOptions } from "react-dropzone";
+import { useDropzone } from "react-dropzone";
+
+import type { Theme } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+const Dropzone = (options: DropzoneOptions) => {
+  const { getRootProps, getInputProps, isDragActive, isDragAccept } =
+      useDropzone(options),
+    handleTheme = (theme: Theme) =>
+      isDragActive
+        ? isDragAccept
+          ? theme.palette.primary.main
+          : theme.palette.error.main
+        : theme.palette.text.disabled;
+
+  return (
+    <Box
+      {...getRootProps()}
+      border="dashed"
+      borderColor={handleTheme}
+      borderRadius={1}
+      my={3}
+      py={8}
+    >
+      <input {...getInputProps()} />
+      <Typography variant="h4" textAlign="center" color={handleTheme}>
+        {isDragActive
+          ? "Drop the files here ..."
+          : `Analysis Score .png .jpg .pdf .gp .mxl`}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Dropzone;

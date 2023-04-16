ea7f4ad54b6dde23c420c489e446a69674edc4f5
izszzz <izszzz@yahoo.co.jp>
2023-04-16 21:41:01 +0900

    feat: resource card transition (#297)

--- src/components/elements/card/square/index.tsx
+++ src/components/elements/card/square/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Box from "@mui/material/Box";
 import CardActionArea from "@mui/material/CardActionArea";
+import Fade from "@mui/material/Fade";
 import type { Route } from "nextjs-routes";
 import { useDarkMode } from "usehooks-ts";
 
@@ -36,26 +37,32 @@ const SquareCard = ({
       <CardActionArea
         sx={{ borderRadius: "5px", height: "inherit", width: "inherit" }}
       >
-        <Box
-          alignItems="center"
-          bgcolor={isDarkMode ? "grey.800" : "grey.300"}
-          borderRadius="5px"
-          display="flex"
-          height="100%"
-          justifyContent="center"
-          sx={{ aspectRatio: "1 / 1" }}
-        >
-          {image ? (
-            <Image
-              alt="image"
-              src={image}
-              style={{ borderRadius: "5px" }}
-              width="100%"
-            />
-          ) : (
-            icon
-          )}
-        </Box>
+        <Fade in={true}>
+          <Box
+            alignItems="center"
+            bgcolor={isDarkMode ? "grey.800" : "grey.300"}
+            borderRadius="5px"
+            display="flex"
+            height="100%"
+            justifyContent="center"
+            sx={{ aspectRatio: "1 / 1" }}
+          >
+            {image ? (
+              <Image
+                alt="image"
+                src={image}
+                style={{
+                  transition: "opacity 0.1s",
+                  opacity: 1,
+                  borderRadius: "5px",
+                }}
+                width="100%"
+              />
+            ) : (
+              icon
+            )}
+          </Box>
+        </Fade>
       </CardActionArea>
       {title}
     </Box>

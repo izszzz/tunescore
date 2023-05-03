c5d25be6fb97061105c625d066f8dd1a50d22126
izszzz <izszzz@yahoo.co.jp>
2023-05-03 19:14:15 +0900

    314 e2e test (#318)
    * chore: toriaezu
    
    * chore: add cypress authorize
    
    * chore: remove cypress & add playwright & auth playwright success
    
    * chore: fix type
    
    ---------
    
    Co-authored-by: k.yasoda <k.yasoda@pid-corp.jp>

---
+++ tests/e2e/setup/token.ts
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from "uuid";
+export const token = {
+  name: "hakei prod",
+  email: "hakeiprod@gmail.com",
+  picture:
+    "https://lh3.googleusercontent.com/a/AGNmyxYIOkNTLwr51sS34gRr3LNBlywwynKhZypxiZ1v=s96-c",
+  sub: "54826c8e-24c8-4b2c-9663-31d69b250325",
+  user: {
+    id: "54826c8e-24c8-4b2c-9663-31d69b250325",
+    name: "hakei prod",
+    email: "hakeiprod@gmail.com",
+    emailVerified: null,
+    image:
+      "https://lh3.googleusercontent.com/a/AGNmyxYIOkNTLwr51sS34gRr3LNBlywwynKhZypxiZ1v=s96-c",
+    stripeCustomerId: null,
+    role: "User",
+    point: 0,
+    providers: [],
+  },
+  iat: 1683095063,
+  exp: 1685687063,
+  jti: uuidv4(),
+};

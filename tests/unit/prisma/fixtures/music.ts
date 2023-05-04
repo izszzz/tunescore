c7bd598f511d163fb36eb4bc332e6c1e82c40002
k.yasoda <k.yasoda@pid-corp.jp>
2023-05-04 22:00:50 +0000

    chore: remove music factory

--- tests/unit/prisma/fixtures/music.ts
+++
@@ -1,8 +0,0 @@
-import type { Prisma } from "@prisma/client";
-import * as Factory from "factory.ts";
-
-export const musicFactory = Factory.Sync.makeFactory<Prisma.MusicCreateInput>({
-  type: "COPY",
-  visibillity: "PUBLIC",
-  resource: { create: { unionType: "Music" } },
-});
